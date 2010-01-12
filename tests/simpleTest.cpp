2009e038b3ba3294531fef1f87524c1d00c0ed82
Matthias Bach <marix@marix.org>
2010-01-12 11:41:01 +0100

    Added a simple test case

---
+++ tests/simpleTest.cpp
@@ -0,0 +1,29 @@
+/**
+ * The most basic test for Einhard
+ *
+ * Copyright 2010 Matthias Bach <marix@marix.org>
+ *
+ * This file is part of Einhard.
+ * 
+ * Einhard is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Einhard is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Einhard.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <einhard/einhard.hpp>
+
+int main( int argc, char* argv[] )
+{
+	// No news are good news
+	return 0;
+}
+
