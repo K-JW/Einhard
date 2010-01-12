e618a03751896de44167ae7c0902fc7ec097e0b6
Matthias Bach <marix@marix.org>
2010-01-12 15:28:23 +0100

    Basic logging functionality

--- include/einhard/einhard.hpp
+++ include/einhard/einhard.hpp
@@ -18,3 +18,179 @@
  * You should have received a copy of the GNU General Public License
  * along with Einhard.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include <iostream>
+
+namespace einhard
+{
+	static char const * const VERSION = "0.1";
+
+	enum LogLevel{ ALL, TRACE, DEBUG, INFO, WARN, ERROR, FATAL, OFF };
+
+	/**
+	 * A utility class that does not print anywhere.
+	 * This allows to not print stuff.
+	 */
+	class OutputFormatter
+	{
+		private:
+			std::ostream * const out;
+
+		public:
+			OutputFormatter( std::ostream * const out ) : out( out ) { };
+
+			template<typename T> inline const OutputFormatter& operator<<( T arg ) const
+			{
+				if( out != 0 )
+					*out << arg;
+
+				return *this;
+			}
+			
+			~OutputFormatter( )
+			{
+				if( out != 0 )
+					*out << std::endl; // TODO this would probably better be only '\n' as to not flush the buffers
+			}
+	};
+
+	template<LogLevel MAX = ALL> class Logger
+	{
+		private:
+			LogLevel verbosity;
+
+		public:
+			Logger( const LogLevel verbosity = WARN ) : verbosity( verbosity ) { };
+
+			const OutputFormatter trace();
+			const OutputFormatter debug();
+			const OutputFormatter info();
+			const OutputFormatter warn();
+			const OutputFormatter error();
+			const OutputFormatter fatal();
+
+			bool beTrace();
+			bool beDebug();
+			bool beInfo();
+			bool beWarn();
+			bool beError();
+			bool beFatal();
+
+			void setVerbosity( LogLevel verbosity )
+			{
+				this->verbosity = verbosity;
+			}
+			LogLevel getVerbosity( )
+			{
+				return this->verbosity;
+			}
+			char const * const getVerbosityString( )
+			{
+				switch( this->verbosity )
+				{
+					case ALL:
+						return "all";
+					case TRACE:
+						return "trace";
+					case DEBUG:
+						return "debug";
+					case INFO:
+						return "info";
+					case WARN:
+						return "warn";
+					case ERROR:
+						return "error";
+					case FATAL:
+						return "fatal";
+					case OFF:
+						return "off";
+					default:
+						return "--- Something is horribly broken - A value outside the enumation has been given ---";
+				}
+			}
+	};
+
+	/*
+	 * IMPLEMENTATIONS
+	 */
+
+	template<LogLevel MAX> const OutputFormatter Logger<MAX>::trace()
+	{
+		if( beTrace() )
+			return OutputFormatter( &std::cout );
+		else
+			return OutputFormatter( 0 );
+	}
+
+	template<LogLevel MAX> bool Logger<MAX>::beTrace()
+	{
+		return ( MAX <= TRACE && verbosity <= TRACE );
+	}
+
+	template<LogLevel MAX> const OutputFormatter Logger<MAX>::debug()
+	{
+		if( beDebug() )
+			return OutputFormatter( &std::cout );
+		else
+			return OutputFormatter( 0 );
+	}
+
+	template<LogLevel MAX> bool Logger<MAX>::beDebug()
+	{
+		return ( MAX <= DEBUG && verbosity <= DEBUG );
+	}
+
+	template<LogLevel MAX> const OutputFormatter Logger<MAX>::info()
+	{
+		if( beInfo() )
+			return OutputFormatter( &std::cout );
+		else
+			return OutputFormatter( 0 );
+	}
+
+	template<LogLevel MAX> bool Logger<MAX>::beInfo()
+	{
+		return ( MAX <= INFO && verbosity <= INFO );
+	}
+
+	template<LogLevel MAX> const OutputFormatter Logger<MAX>::warn()
+	{
+		if( beWarn() )
+			return OutputFormatter( &std::cout );
+		else
+			return OutputFormatter( 0 );
+	}
+
+	template<LogLevel MAX> bool Logger<MAX>::beWarn()
+	{
+		return ( MAX <= WARN && verbosity <= WARN );
+	}
+
+	template<LogLevel MAX> const OutputFormatter Logger<MAX>::error()
+	{
+		if( beError() )
+			return OutputFormatter( &std::cout );
+		else
+			return OutputFormatter( 0 );
+	}
+
+	template<LogLevel MAX> bool Logger<MAX>::beError()
+	{
+		return ( MAX <= ERROR && verbosity <= ERROR );
+	}
+
+	template<LogLevel MAX> const OutputFormatter Logger<MAX>::fatal()
+	{
+		if( beFatal() )
+			return OutputFormatter( &std::cout );
+		else
+			return OutputFormatter( 0 );
+	}
+
+	template<LogLevel MAX> bool Logger<MAX>::beFatal()
+	{
+		return ( MAX <= FATAL && verbosity <= FATAL );
+	}
+
+};
+
--- tests/simpleTest.cpp
+++ tests/simpleTest.cpp
@@ -21,8 +21,58 @@
 
 #include <einhard/einhard.hpp>
 
+using namespace einhard;
+
 int main( int argc, char* argv[] )
 {
+	Logger<> baseLogger;
+
+	baseLogger.trace() << "Trace output";
+	baseLogger.trace() << 2 << " times traced";
+	baseLogger.debug() << "Debug output";
+	baseLogger.info() << "Info output";
+	baseLogger.warn() << "Warning output";
+	baseLogger.error() << "Error output";
+	baseLogger.fatal() << "Fatal output";
+
+	if( baseLogger.beTrace() )
+		return 1;
+	if( baseLogger.beDebug() )
+		return 1;
+	if( baseLogger.beInfo() )
+		return 1;
+	if( ! baseLogger.beWarn() )
+		return 1;
+	if( ! baseLogger.beError() )
+		return 1;
+	if( ! baseLogger.beFatal() )
+		return 1;
+
+	baseLogger.setVerbosity( ALL );
+
+	baseLogger.info() << "The verbosity has now been set to " << baseLogger.getVerbosity() << " which is " << baseLogger.getVerbosityString();
+
+	baseLogger.trace() << "Trace output";
+	baseLogger.trace() << 2 << " times traced";
+	baseLogger.debug() << "Debug output";
+	baseLogger.info() << "Info output";
+	baseLogger.warn() << "Warning output";
+	baseLogger.error() << "Error output";
+	baseLogger.fatal() << "Fatal output";
+
+	if( ! baseLogger.beTrace() )
+		return 1;
+	if( ! baseLogger.beDebug() )
+		return 1;
+	if( ! baseLogger.beInfo() )
+		return 1;
+	if( ! baseLogger.beWarn() )
+		return 1;
+	if( ! baseLogger.beError() )
+		return 1;
+	if( ! baseLogger.beFatal() )
+		return 1;
+
 	// No news are good news
 	return 0;
 }
